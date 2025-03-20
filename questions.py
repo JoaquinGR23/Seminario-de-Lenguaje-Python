0d7306933f6fc6c7c36c520ac5eb1c0de0868f96
joaquin <gimenezruizjoa@gmail.com>
2025-03-20 13:48:04 -0300

    sin preguntas repetidas

--- questions.py
+++ questions.py
@@ -28,7 +28,7 @@ correct_answers_index = [1, 2, 0, 3, 1]
 score = 0
 
 # seleccion de 3 preguntas al azar
-questions_to_ask = random.choices(list(zip(questions,answers, correct_answers_index)), k=3)
+questions_to_ask = random.sample(list(zip(questions,answers, correct_answers_index)), k=3)
 
 # El usuario deberá contestar 3 preguntas
 for question, answer_list, correct_answer_index in questions_to_ask:
